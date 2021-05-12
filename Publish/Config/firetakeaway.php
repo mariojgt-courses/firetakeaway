02e83c959899d7052bbd3f60137cf1924aa9e183
mario jose goes tarosso <mariojgt2@gmail.com>
2021-05-12 19:26:35 +0100

    config file up

--- Publish/Config/firetakeaway.php
+++ Publish/Config/firetakeaway.php
@@ -2,7 +2,9 @@
 
 return [
     'version'         => '1.0.3',   // site version
-    'register_enable' => false,     // if true users can register
+    'register_enable' => true,     // if true users can register
     // Needed for the laravel sanctum
     'boot_token'           => 'skeleton_default_token',
+    'default_status' => 1,
+    'confirm_status' => 2
 ];
